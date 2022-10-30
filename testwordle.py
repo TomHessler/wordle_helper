8c9d3a0a22db72b1167bac0a77d9a0daee93b415
Tom <tohe5865@student.su.se>
2022-10-30 13:16:27 +0100

    fix test and add gui function

--- testwordle.py
+++ testwordle.py
@@ -1,13 +1,13 @@
 from wordle_helper import grey, green, yellow
 
-words = ['hello', 'phone','ghast','arise']
+words = ['hello', 'phone','ghost','hallo','senap']
 
-assert grey('a', words) == ['hello', 'phone']
-assert grey('e', words) == ['ghast']
+assert grey('a', words) == ['hello', 'phone','ghost']
+assert grey('e', words) == ['ghost', 'hallo']
 print('grey success')
-assert yellow('e', words) == ['phone','arise']
-assert yellow('a', words) == ['arise']
+assert yellow('e',1, words) == ['phone']
+assert yellow('a',2, words) == ['hallo','senap']
 print('yellow success')
-assert green('e', 1, words) == ['hello']
-assert green('a', 2, words) == ['ghast']
+assert green('e', 1, words) == ['hello','senap']
+assert green('a', 2, words) == []
 print('green success')
--- wordle_helper.py
+++ wordle_helper.py
@@ -1,7 +1,5 @@
 from tkinter import *
 
-window = Tk()
-
 def grey(letter, word_list):
     return [word for word in word_list if not letter in word]
 
@@ -52,62 +50,69 @@ def get_result():
     
     text.insert(END, words)
 
-window.title('Wordle Helper')
-window.geometry("400x600")
+def GUI():
 
-label1 = Label(window, text="Enter letters in correct positions:",font = ('Verdana',15))
-label1.place(x=30,y=10)
-
-entry1 = Entry(window,bg="green",font = ('Verdana',30))
-entry2 = Entry(window,bg="green",font = ('Verdana',30))
-entry3 = Entry(window,bg="green",font = ('Verdana',30))
-entry4 = Entry(window,bg="green",font = ('Verdana',30))
-entry5 = Entry(window,bg="green",font = ('Verdana',30))
-
-entry1.place(x=100-25,y=50,width=50,height=50)
-entry2.place(x=150-25,y=50,width=50,height=50)
-entry3.place(x=200-25,y=50,width=50,height=50)
-entry4.place(x=250-25,y=50,width=50,height=50)
-entry5.place(x=300-25,y=50,width=50,height=50)
-
-label2 = Label(window, text="Enter yellow letters:",font = ('Verdana',15))
-label2.place(x=90,y=110)
-
-entry6 = Entry(window,bg="yellow",font = ('Verdana',30))
-entry7 = Entry(window,bg="yellow",font = ('Verdana',30))
-entry8 = Entry(window,bg="yellow",font = ('Verdana',30))
-entry9 = Entry(window,bg="yellow",font = ('Verdana',30))
-entry10 = Entry(window,bg="yellow",font = ('Verdana',30))
-
-entry6.place(x=100-25,y=150,width=50,height=50)
-entry7.place(x=150-25,y=150,width=50,height=50)
-entry8.place(x=200-25,y=150,width=50,height=50)
-entry9.place(x=250-25,y=150,width=50,height=50)
-entry10.place(x=300-25,y=150,width=50,height=50)
-
-# entry_yellow = Entry(window,bg="yellow",font = ('Verdana',30))
-# entry_yellow.place(x=50,y=150,width=300,height=50)
-
-label3 = Label(window, text="Enter grey letters:",font = ('Verdana',15))
-label3.place(x=100,y=210)
-
-entry_grey = Entry(window,bg="grey",font = ('Verdana',30))
-entry_grey.place(x=50,y=250,width=300,height=50)
+    window = Tk()
     
-
-button = Button(window,width=8,height=1,text ="Get Words",command = get_result)
-button.place(x=165,y=325)
-
-text = Text(window)
-text.place(x=50,y=380,width=300,height=185)
-
-scrollbar = Scrollbar(orient=VERTICAL)
+    window.title('Wordle Helper')
+    window.geometry("400x600")
     
-scrollbar.config(command=text.yview)
-text.config(yscrollcommand=scrollbar.set)
-scrollbar.place(x=335,y=380,height=180)
-
-label_count_words = Label(window)
-label_count_words.place(x=50,y=570)
-
-window.mainloop()
+    label1 = Label(window, text="Enter letters in correct positions:",font = ('Verdana',15))
+    label1.place(x=30,y=10)
+    
+    entry1 = Entry(window,bg="green",font = ('Verdana',30))
+    entry2 = Entry(window,bg="green",font = ('Verdana',30))
+    entry3 = Entry(window,bg="green",font = ('Verdana',30))
+    entry4 = Entry(window,bg="green",font = ('Verdana',30))
+    entry5 = Entry(window,bg="green",font = ('Verdana',30))
+    
+    entry1.place(x=100-25,y=50,width=50,height=50)
+    entry2.place(x=150-25,y=50,width=50,height=50)
+    entry3.place(x=200-25,y=50,width=50,height=50)
+    entry4.place(x=250-25,y=50,width=50,height=50)
+    entry5.place(x=300-25,y=50,width=50,height=50)
+    
+    label2 = Label(window, text="Enter yellow letters:",font = ('Verdana',15))
+    label2.place(x=90,y=110)
+    
+    entry6 = Entry(window,bg="yellow",font = ('Verdana',30))
+    entry7 = Entry(window,bg="yellow",font = ('Verdana',30))
+    entry8 = Entry(window,bg="yellow",font = ('Verdana',30))
+    entry9 = Entry(window,bg="yellow",font = ('Verdana',30))
+    entry10 = Entry(window,bg="yellow",font = ('Verdana',30))
+    
+    entry6.place(x=100-25,y=150,width=50,height=50)
+    entry7.place(x=150-25,y=150,width=50,height=50)
+    entry8.place(x=200-25,y=150,width=50,height=50)
+    entry9.place(x=250-25,y=150,width=50,height=50)
+    entry10.place(x=300-25,y=150,width=50,height=50)
+    
+    # entry_yellow = Entry(window,bg="yellow",font = ('Verdana',30))
+    # entry_yellow.place(x=50,y=150,width=300,height=50)
+    
+    label3 = Label(window, text="Enter grey letters:",font = ('Verdana',15))
+    label3.place(x=100,y=210)
+    
+    entry_grey = Entry(window,bg="grey",font = ('Verdana',30))
+    entry_grey.place(x=50,y=250,width=300,height=50)
+        
+    
+    button = Button(window,width=8,height=1,text ="Get Words",command = get_result)
+    button.place(x=165,y=325)
+    
+    text = Text(window)
+    text.place(x=50,y=380,width=300,height=185)
+    
+    scrollbar = Scrollbar(orient=VERTICAL)
+        
+    scrollbar.config(command=text.yview)
+    text.config(yscrollcommand=scrollbar.set)
+    scrollbar.place(x=335,y=380,height=180)
+    
+    label_count_words = Label(window)
+    label_count_words.place(x=50,y=570)
+    
+    window.mainloop()
+    
+if __name__ == '__main__':
+    GUI()
