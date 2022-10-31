084163f0c8ac4697b12e7b0200c494aec7727150
Tom <tohe5865@student.su.se>
2022-10-31 15:15:15 +0100

    Update simulation.py

--- simulation.py
+++ simulation.py
@@ -15,25 +15,31 @@ def result(guess, answer):
 
 def simulate_game(answer, word_list):
     trials = 0
+    fails = 0
     while True:
         trials += 1
+        if trials>6:
+            fails += 1
+            break
         guess = suggest_word(word_list).strip("\n")
         if guess == answer.strip('\n'):
             break
         color_list = result(guess, answer)
         for index, color in enumerate(color_list):
             word_list = color(guess[index], index, word_list)
-    return trials
+    return trials, fails
 
 
 def simulation(word_list, answer_list):
-    total = 0
-
+    total_trails = 0
+    total_fails = 0
     for answer in answer_list:
-        total += simulate_game(answer, word_list)
-
-    avg_number_guesses = total / len(answer_list)
-    return avg_number_guesses
+        trials, fails = simulate_game(answer, word_list)
+        total_trails += trials
+        total_fails += fails
+    avg_number_guesses = total_trails / len(answer_list)
+    success_rate = 1 - total_fails / len(answer_list)
+    return avg_number_guesses, success_rate
 
 
 if __name__ == "__main__":
@@ -51,9 +57,7 @@ if __name__ == "__main__":
             word_list.append(word)
         word_list_f.close()
 
-    print(
-        f"average number of guesses without knowing answer list: {simulation(word_list, answer_list)}"
-    )
-    print(
-        f"average number of guesses given answer list: {simulation(answer_list, answer_list)}"
-    )
+    avg_number_guesses, success_rate = simulation(answer_list, answer_list)
+
+    print(f"average number of guesses: {avg_number_guesses}")
+    print(f"success rate: {success_rate}")
